606a2cfcd6c74033ddb0b07296549a624f88da95
Jeanlui21 <pamui_2121@hotmail.com>
2021-03-24 22:29:32 -0500

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  inventoryDB: 'https://inventarioapp-2f2a2-default-rtdb.firebaseio.com',
+  firebase: {
+    apiKey: 'AIzaSyD72ThS_pAWxwR9Ao-mif4u7U5itcXH4F0',
+    authDomain: 'inventarioapp-2f2a2.firebaseapp.com',
+    databaseURL: 'https://inventarioapp-2f2a2-default-rtdb.firebaseio.com',
+    projectId: 'inventarioapp-2f2a2',
+    storageBucket: 'inventarioapp-2f2a2.appspot.com',
+    messagingSenderId: '126981880199',
+    appId: '1:126981880199:web:04748ef124085a3ed8fb22',
+    measurementId: 'G-R3MGYE8RM3'
+  },
   production: true
 };
