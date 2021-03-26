dec907577d8fe9756ac9efb024be5c492703218c
Jeanlui21 <pamui_2121@hotmail.com>
2021-03-25 22:39:28 -0500

    descargar datos completado

--- src/app/components/item-download/item-download.component.ts
+++ src/app/components/item-download/item-download.component.ts
@@ -5,6 +5,10 @@ import {ProductoService} from '@app/services/producto.service';
 import {SalesService} from '@app/services/sales.service';
 import {ExportExcelService} from '@app/services/export-excel.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+// IMPORTS
+// @ts-ignore
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-item-download',
@@ -29,6 +33,32 @@ export class ItemDownloadComponent implements OnInit {
     });
   }
 
+  downloadPdf(): void {
+    // Extraemos el
+    const DATA = document.getElementById('htmlToExport');
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    html2canvas(DATA, options).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+
+      // Add image Canvas to PDF
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`inventario-${this.Form.get('tabla').value}-${new Date().getDate()
+      + '-' + (new Date().getMonth() + 1) + '-' + new Date().getFullYear()}`);
+    });
+  }
+
   exportToExcel(): void {
     const toExcel = [...this.listExport];
     toExcel.forEach(product => {
@@ -36,7 +66,7 @@ export class ItemDownloadComponent implements OnInit {
       delete product.id;
     });
     const date = new Date().getDate() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getFullYear();
-    this.serviceExcel.exportExcel(toExcel, 'inventario-' + this.Form.get('tabla').value + + date);
+    this.serviceExcel.exportExcel(toExcel, 'inventario-' + this.Form.get('tabla').value + +date);
   }
 
   listAttributes(): void {
